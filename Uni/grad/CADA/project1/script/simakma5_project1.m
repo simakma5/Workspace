e6450407b96eb4cd95411374aebe37cdb6ae70f0
Martin Simak <martin.simaak@gmail.com>
2023-06-02 22:38:06 +0200

    CADA(project1): review final

--- Uni/grad/CADA/project1/script/simakma5_project1.m
+++ Uni/grad/CADA/project1/script/simakma5_project1.m
@@ -16,19 +16,19 @@ c = 299792458;                                  % speed of light
 Nx = 400;                                       % even number of nodes in x
 Ny = 100;                                       % even number of nodes in y
 % Ny/2 - h/2 - 1 > 20*h
-h = min(6,round((Ny-2)/21));                    % dielectric height
+h = min(2.5,round((Ny-2)/21));                  % dielectric height
 if mod(h,2) == 1
     h = h + 1;
 end
 % Nx/2 - g/2 - w/2 > 20*h
-w = min(62,round((Nx-40*h)/2.5));               % strips width
+w = min(58,round((Nx-40*h)/2.5));               % strips width
 if w <= 0
     error('The ratio Nx/Ny is too low')
 end
 if mod(w,2) == 1
     w = w + 1;
 end
-g = round(1.5*w);                               % gap between strips' centres
+g = round(2*w);                                 % gap between strips' centres
 if mod(g,2) == 1
     g = g + 1;
 end
@@ -40,13 +40,13 @@ Vgnd = 0;                                       % ground potential
 eps_r = 3.05;                                   % relative permittivity of the dielectric
 er_avg = (1+eps_r)/2;
 % initial values of potential across the mesh
-V_init = (V0+Vgnd)/2*ones(Ny,Nx);               % free nodes inside the structure
+V_init = zeros(Ny,Nx);                          % free nodes inside the structure
 V_init(1,:) = Vgnd;                             % bottom edge of the bounding box
 V_init(Ny,:) = Vgnd;                            % top edge of the bounding box
 V_init(:,1) = Vgnd;                             % left edge of the bounding box
 V_init(:,Nx) = Vgnd;                            % right edge of the bounding box
-V_init(IF1-1,Nx/2-g/2-w/2:Nx/2-g/2+w/2) = V0;   % left strip
-V_init(IF1-1,Nx/2+g/2-w/2:Nx/2+g/2+w/2) = V0;   % right strip
+V_init(IF1-1,Nx/2-g/2-w/2:Nx/2-g/2+w/2) = V0;   % left strip (V0)
+V_init(IF1-1,Nx/2+g/2-w/2:Nx/2+g/2+w/2) = -V0;  % right strip (-V0)
     
 %% Calculation of potential -> charge -> capacity -> characteristic impedance
 % Step 1: Obtain potential using the method of successive over-relaxation (SOR)
@@ -62,13 +62,13 @@ R = zeros(Ny,Nx);                               % improvements over the last ite
 k = 5;                                          % margin of the integration curve
 left = Nx/2 - g/2 - w/2 - k;
 bottom = IF1 - 1 - k;
-right = Nx/2 + g/2 + w/2 + k;
+right = Nx/2 - g/2 + w/2 + k;
 top = IF1 - 1 + k;
 
 tic
 alpha_step = 0.01;
 alpha_range = 1:alpha_step:1.99;                % examined values of alpha in [1,2)
-alpha_range = 1.7;                              % single value for debugging with nice fig2
+alpha_range = 1.94;                             % single value for debugging with nice fig2 (1.7)
 alpha_iter = ones(1,length(alpha_range));       % number of iterations for given alpha
 for alpha = alpha_range                         % calculate everything for different alphas
     Rmax = V0;                                  % largest point improvement across the mesh
@@ -79,20 +79,25 @@ for alpha = alpha_range                         % calculate everything for diffe
         % Step 1: SOR method
         for j = 2:Nx-1
             for i = 2:Ny-1
-                if i == IF1                     % nodes on interface 1
+                % nodes on interface 1
+                if i == IF1
                     R(i,j) = (eps_r*V(i+1,j)+1*V(i-1,j)+er_avg*V(i,j-1)+er_avg*V(i,j+1))/4/er_avg-V(i,j); 
-                elseif i == IF2                 % nodes on interface 2
-                    R(i,j) = (1*V(i+1,j)+eps_r*V(i-1,j)+er_avg*V(i,j-1)+er_avg*V(i,j+1))/4/er_avg-V(i,j); 
-                else                            % free nodes
+                % nodes on interface 2
+                elseif i == IF2
+                    R(i,j) = (1*V(i+1,j)+eps_r*V(i-1,j)+er_avg*V(i,j-1)+er_avg*V(i,j+1))/4/er_avg-V(i,j);
+                % nodes in strips
+                elseif i == IF1-1 && ((j >= Nx/2-g/2-w/2 && j <= Nx/2-g/2+w/2) || (j >= Nx/2+g/2-w/2 && j <= Nx/2+g/2+w/2))
+                    R(i,j)= R(i,j);
+                    V(i,j)= V(i,j);
+                % free nodes
+                else
                     R(i,j) = (V(i+1,j)+V(i-1,j)+V(i,j-1)+V(i,j+1))/4-V(i,j);
                 end
-                    V(i,j) = V(i,j)+alpha*R(i,j);
-                V(IF1-1,Nx/2-g/2-w/2:Nx/2-g/2+w/2) = V0;
-                V(IF1-1,Nx/2+g/2-w/2:Nx/2+g/2+w/2) = V0;
+                V(i,j) = V(i,j)+alpha*R(i,j);
             end
         end
         Rmax = max(max(R));
-        % Step 2: numerical evaluation of the intergral along the defined curve
+        % Step 2: numerical evaluation of the integral along the curve
         qA = 0; qB = 0; qC = 0; qD = 0;
         for i=top:1:bottom
             if i < IF1
@@ -132,15 +137,14 @@ quiver(Ex,Ey);
 % plot settings
 xticklabels(round((xticks-Nx/2)./h))
 yticklabels(round((yticks-Ny/2)./h))
-xlabel('x [mm]')
-ylabel('y [mm]')
+xlabel('x [mm]','FontSize',18)
+ylabel('y [mm]','FontSize',18)
 colorbar
 % colormap winter
 % colormap jet
-title(['Q = ' num2str(round(Q(end)*1e9,2)) ...
-    ' nC/m, C = ' num2str(round(C(end)*1e9,3)) ...
-    ' nF/m, Z = ' num2str(round(Z(end),1)) ' \Omega'])
-subtitle(['w = ' num2str(round(w/h,2)) ' mm, g = ' num2str(round(g/h,2)) ' mm'])
+title(['Z = ' num2str(round(Z(end),1)) ...
+    ' \Omega, C = ' num2str(round(C(end)*1e9,3)) ' nF/m'],'FontSize',18)
+subtitle(['w = ' num2str(round(w/h,2)) ' mm, g = ' num2str(round(g/h,2)) ' mm'],'FontSize',18)
 % interface 1
 if1_up = yline(IF1);
 if1_up.Color = [0.4 0.4 0.4];
@@ -173,19 +177,41 @@ hold off
 figure(2)
 plot(Z)
 axis tight
-ylabel('Z_0 [\Omega]')
-xlabel('Number of iterations [-]')
+ylabel('Z_0 [\Omega]','FontSize',18)
+xlabel('Number of iterations [-]','FontSize',18)
+ylim([0 100])
 % Z0 = 50 line
 if1_up = yline(50);
 if1_up.Color = [0.4 0.4 0.4];
 if1_up.LineStyle = '--';
+
+%
+figure(3)
+plot(Ey(IF1-1,Nx/2-g/2-w/2:Nx/2-g/2+w/2))
+axis tight
+xticklabels(round((xticks-w/2)./h))
+xlabel('Strip-centered x-coordinate [mm]','FontSize',18)
+ylabel('Charge density','FontSize',18)
 
 % Compare numerically optimal value of alpha with the analytical solution
 r = cos(pi/Nx) + cos(pi/Ny);
 alpha_optimal = (8-sqrt(64-16*r^2))/r^2;
 disp(['Analytical solution for the optimal value of alpha: ' num2str(round(alpha_optimal,2))])
 disp(['Numerically determined optimal value of alpha: ' num2str(alpha_range(alpha_iter==min(alpha_iter)))])
-% Full run (alpha = 1:0.01:1.9) results:
+
+% Plot the dependecy of the number of iterations for given alpha 
+figure(4)
+plot(alpha_range,alpha_iter)
+axis tight
+ylabel('Number of iterations [-]','FontSize',18)
+xlabel('SOR coefficient \alpha [-]','FontSize',18)
+
+% Full run (alpha = 1:0.01:1.9) old results:
 % Elapsed time is 3969.8 seconds.
 % Analytical solution for the optimal value of alpha: 1.96
 % Numerically determined optimal value of alpha: 1.91
+
+% Full run (alpha = 1:0.01:1.9) new results:
+% Elapsed time is 70.186527 seconds.
+% Analytical solution for the optimal value of alpha: 1.96
+% Numerically determined optimal value of alpha: 1.94
