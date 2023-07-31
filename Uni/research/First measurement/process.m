737bdb505310e91b1fbf72b6cf7c4e7c7985867d
Martin Simak <martin.simaak@gmail.com>
2023-07-31 22:35:09 +0200

    research: RF toolbox functions, overtravel compensation, refactoring

--- Uni/research/First measurement/process.m
+++ Uni/research/First measurement/process.m
@@ -3,34 +3,71 @@ close all
 clc
 
 %% initialization
+c = 299792458;
 MEASUREMENT = ["metal" "ceramics" "absorber"];
 
+% ISS CPW parameters
+ConductorWidth = 45e-6;         % measured
+EpsilonR = 9.9;                 % nominal
+Height = 0.254e-3;              % nominal
+SlotWidth = 30e-6;              % measured
+Thickness = 1e-6;               % estimated (tune)
+
 % models of calibration standards
-LShort = 2.4e-12;
-COpen = -9.3e-15;
-LMatch = -3.5e-12;
-RMatch = 50;
+ShortL = 2.4e-12;
+OpenC = -9.3e-15;
+MatchL = -3.5e-12;
+MatchR = 50;
 
-ThruLength = 200e-6;        % Thru length (physical, measured) 
-ThruDelay = 1e-12;          % Thru delay (nominal)
-Line1Delay = 3e-12;         % CPW Line 1 delay (nominal)
-Line2Delay = 7e-12;         % CPW Line 2 delay (nominal)
-Line3Delay = 14e-12;        % CPW Line 3 delay (nominal)
-Line4Delay = 27e-12;        % CPW Line 4 delay (nominal)
-Line5Delay = 40e-12;        % CPW Line 5 delay (nominal)
+% calibration Thru parameters
+ThruDelay = 1e-12;              % nominal delay
+ThruLength = 200e-6;            % measured physical length
 
-Z0 = 50;
+% verification Line{1..5} nominal parameters from datasheet
+Line1Delay = 3e-12;
+Line1Length = 446e-6;
+Line2Delay = 7e-12;
+Line2Length = 896e-6;
+Line3Delay = 14e-12;
+Line3Length = 1796e-6;
+Line4Delay = 27e-12;
+Line4Length = 3496e-6;
+Line5Delay = 40e-12;
+Line5Length = 5246e-6;
+
+LineVelocity = [
+    Line1Length/Line1Delay
+    Line2Length/Line2Delay
+    Line3Length/Line3Delay
+    Line4Length/Line4Delay
+    Line5Length/Line5Delay
+];
+% Eff. diel. const. for 1um thick gold plating using txline: 5.32304 @ 30 GHz
+LineEpsilon = (c./LineVelocity).^2;
+
+overtravel = 50e-6;             % estimate from microscope image
+% effective parameters accounting for referance plane shift by overtravel
+Line1Delay = Line1Delay - overtravel/LineVelocity(1);
+Line1Length = Line1Length - overtravel;
+Line2Delay = Line2Delay - overtravel/LineVelocity(2);
+Line2Length = Line2Length - overtravel;
+Line3Delay = Line3Delay - overtravel/LineVelocity(3);
+Line3Length = Line3Length - overtravel;
+Line4Delay = Line4Delay - overtravel/LineVelocity(4);
+Line4Length = Line4Length - overtravel;
+Line5Delay = Line5Delay - overtravel/LineVelocity(5);
+Line5Length = Line5Length - overtravel;
+
 % arbitrarily chosen measurement only to obtain frequency points
 freq = SXPParse(fullfile('metal', 'Open.s2p'));
-nFreq = length(freq);
 
 %% models of standards
-ShortImp = 1j*2*pi*freq*LShort;
-ShortRefl = (ShortImp - Z0)./(ShortImp + Z0);
-OpenImp = 1./(1j*2*pi*freq*COpen);
-OpenRefl = (OpenImp - Z0)./(OpenImp + Z0);
-MatchImp = 50 + 1j*2*pi*freq*LMatch;
-MatchRefl = (MatchImp - Z0)./(MatchImp + Z0);
+ShortImp = 1j*2*pi*freq*ShortL;
+ShortRefl = (ShortImp - MatchR)./(ShortImp + MatchR);
+OpenImp = 1./(1j*2*pi*freq*OpenC);
+OpenRefl = (OpenImp - MatchR)./(OpenImp + MatchR);
+MatchImp = 50 + 1j*2*pi*freq*MatchL;
+MatchRefl = (MatchImp - MatchR)./(MatchImp + MatchR);
 
 % reflection coeficient of ports
 Port1Model = struct( ...
@@ -40,24 +77,55 @@ Port1Model = struct( ...
 );
 Port2Model = Port1Model;
 
-% model of Thru
-cpwThru = txlineCPW( ...
-    'ConductorWidth', 45e-6, ...
-    'EpsilonR', 9.9, ...
-    'Height', 0.254e-3, ...
-    'SlotWidth', 30e-6, ...
-    'Thickness', 1e-6, ...
+% CPW models of standards
+ThruModel = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
     'LineLength', ThruLength ...
-);
-thruS21Model = squeeze(sparameters(cpwThru, freq).Parameters(2, 1, :));
-
-% lossless models of Thru nad Line{1..5} based on their nominal delays
-SIdealThru = transmLineSPar(shiftdim(1j*2*pi*freq*ThruDelay, -2), 1, Z0, Z0);
-SIdealLine1 = transmLineSPar(shiftdim(1j*2*pi*freq*Line1Delay, -2), 1, Z0, Z0);
-SIdealLine2 = transmLineSPar(shiftdim(1j*2*pi*freq*Line2Delay, -2), 1, Z0, Z0);
-SIdealLine3 = transmLineSPar(shiftdim(1j*2*pi*freq*Line3Delay, -2), 1, Z0, Z0);
-SIdealLine4 = transmLineSPar(shiftdim(1j*2*pi*freq*Line4Delay, -2), 1, Z0, Z0);
-SIdealLine5 = transmLineSPar(shiftdim(1j*2*pi*freq*Line5Delay, -2), 1, Z0, Z0);
+), freq);
+Line1Model = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
+    'LineLength', Line1Length ...
+), freq);
+Line2Model = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
+    'LineLength', Line2Length ...
+), freq);
+Line3Model = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
+    'LineLength', Line3Length ...
+), freq);
+Line4Model = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
+    'LineLength', Line4Length ...
+), freq);
+Line5Model = sparameters(txlineCPW( ...
+    'ConductorWidth', ConductorWidth, ...
+    'EpsilonR', EpsilonR, ...
+    'Height', Height, ...
+    'SlotWidth', SlotWidth, ...
+    'Thickness', Thickness, ...
+    'LineLength', Line5Length ...
+), freq);
 
 %% calibration on metal - phase error inspection
 for m = 1:3
@@ -65,7 +133,7 @@ for m = 1:3
     [~, P1MeasMetal, P2MeasMetal, ThruMeasMetal, SLineMeasMetal] = loadData(measurement);
     
     [EaMetal, EbMetal] = UOSM(P1MeasMetal, P2MeasMetal, ThruMeasMetal, ...
-        Port1Model, Port2Model, squeeze(SIdealThru(2, 1, :)), freq);
+        Port1Model, Port2Model, squeeze(ThruModel.Parameters(2, 1, :)), freq);
     
     ThruCalMetal = calibrate2PortSParDUT(EaMetal, EbMetal, ThruMeasMetal);
     Line1CalMetal = calibrate2PortSParDUT(EaMetal, EbMetal, SLineMeasMetal(:, :, :, 1));
@@ -76,13 +144,13 @@ for m = 1:3
     
     figure(1)
     subplot(str2double(strcat(num2str(13), num2str(m))))
-    plot(freq/1e9, unwrapPhase(squeeze(angle(ThruCalMetal(2, 1, :)./SIdealThru(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(ThruCalMetal(2, 1, :)./ThruModel.Parameters(2, 1, :)))/pi*180))
     hold on
-    plot(freq/1e9, unwrapPhase(squeeze(angle(Line1CalMetal(2, 1, :)./SIdealLine1(2, 1, :)))/pi*180))
-    plot(freq/1e9, unwrapPhase(squeeze(angle(Line2CalMetal(2, 1, :)./SIdealLine2(2, 1, :)))/pi*180))
-    plot(freq/1e9, unwrapPhase(squeeze(angle(Line3CalMetal(2, 1, :)./SIdealLine3(2, 1, :)))/pi*180))
-    plot(freq/1e9, unwrapPhase(squeeze(angle(Line4CalMetal(2, 1, :)./SIdealLine4(2, 1, :)))/pi*180))
-    plot(freq/1e9, unwrapPhase(squeeze(angle(Line5CalMetal(2, 1, :)./SIdealLine5(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(Line1CalMetal(2, 1, :)./Line1Model.Parameters(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(Line2CalMetal(2, 1, :)./Line2Model.Parameters(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(Line3CalMetal(2, 1, :)./Line3Model.Parameters(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(Line4CalMetal(2, 1, :)./Line4Model.Parameters(2, 1, :)))/pi*180))
+    plot(freq/1e9, unwrapPhase(angle(squeeze(Line5CalMetal(2, 1, :)./Line5Model.Parameters(2, 1, :)))/pi*180))
     title(['Measurement on ' measurement])
     grid on
     grid minor
@@ -94,13 +162,13 @@ for m = 1:3
 
     figure(2)
     subplot(str2double(strcat(num2str(13), num2str(m))))
-    plot(freq/1e9, squeeze(20*log10(abs(ThruCalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(ThruCalMetal(2, 1, :)))))
     hold on
-    plot(freq/1e9, squeeze(20*log10(abs(Line1CalMetal(2, 1, :)))))
-    plot(freq/1e9, squeeze(20*log10(abs(Line2CalMetal(2, 1, :)))))
-    plot(freq/1e9, squeeze(20*log10(abs(Line3CalMetal(2, 1, :)))))
-    plot(freq/1e9, squeeze(20*log10(abs(Line4CalMetal(2, 1, :)))))
-    plot(freq/1e9, squeeze(20*log10(abs(Line5CalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(Line1CalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(Line2CalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(Line3CalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(Line4CalMetal(2, 1, :)))))
+    plot(freq/1e9, 20*log10(abs(squeeze(Line5CalMetal(2, 1, :)))))
     yline(0)
     title(['Measurement on ' measurement])
     grid on
@@ -126,7 +194,7 @@ hold off
 % measurement = sprintf('%s', MEASUREMENT(m));
 % [~, P1MeasMetal, P2MeasMetal, ThruMeasMetal, SLineMeasMetal] = loadData(measurement);
 % 
-% [EaMetal, EbMetal] = UOSM(P1MeasMetal, P2MeasMetal, ThruMeasMetal, Port1Model, Port2Model, squeeze(SIdealThru(2, 1, :)), freq);
+% [EaMetal, EbMetal] = UOSM(P1MeasMetal, P2MeasMetal, ThruMeasMetal, Port1Model, Port2Model, squeeze(ThruModel(2, 1, :)), freq);
 % 
 % ThruCalMetal = calibrate2PortSParDUT(EaMetal, EbMetal, ThruMeasMetal);
 % Line1CalMetal = calibrate2PortSParDUT(EaMetal, EbMetal, SLineMeasMetal(:, :, :, 1));
@@ -147,7 +215,7 @@ hold off
 %    'LineWidth', [2 2], 'LegendText', {'Ea (S_{@@})', 'Eb (S_{@@})'})
 % 
 % figure(4)
-% showXPortParameters({freq}, {ThruCalMetal, SIdealThru}, 'Title', ['Calibrated Thru ' measurement], ...
+% showXPortParameters({freq}, {ThruCalMetal, ThruModel}, 'Title', ['Calibrated Thru ' measurement], ...
 %    'LineWidth', [2 2], 'LegendText', {'Meas (S_{@@})', 'Ideal (S_{@@})'})
 % 
 % figure(5)
