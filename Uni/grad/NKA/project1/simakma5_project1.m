082007a57fa9091dae76c8de69357fb3d536a21d
simakma5 <martin.simaak@gmail.com>
2024-01-14 23:19:07 +0100

    NKA: project1 refactored

--- Uni/grad/NKA/project1/simakma5_project1.m
+++ Uni/grad/NKA/project1/simakma5_project1.m
@@ -21,52 +21,27 @@ tand = 0.0022;
 fSpan = 1e9;                                    % frequency plots span
 Nfpoints = 5e3+1;                               % number of freq. points
 f = linspace(f_r-fSpan/2, f_r+fSpan/2, Nfpoints);
-NLpoints = 5e3;                                 % number of length points
+NLpoints = 1e3;                                 % number of length points
 
 % 1.1
 % antenna dimensions
-W = c./(2*f)*sqrt(2/(epsR+1));
-epsR_eff = (epsR+1)/2+(epsR-1)/2*(1+12*h./W).^(-1/2);
-deltaL = 0.412*h.*(((epsR_eff+0.3).*(W/h+0.264))./((epsR_eff-0.258).*(W/h+0.813)));
-L_eff = c./(2*f.*sqrt(epsR_eff));
+W = c/(2*f_r)*sqrt(2/(epsR+1));
+epsR_eff = (epsR+1)/2+(epsR-1)/2*(1+12*h/W)^(-1/2);
+deltaL = 0.412*h*(((epsR_eff+0.3)*(W/h+0.264))/((epsR_eff-0.258)*(W/h+0.813)));
+L_eff = c/(2*f_r*sqrt(epsR_eff));
 L = L_eff-2*deltaL;
 fprintf('1.1\n----\n')
 fprintf('f_r = %.2f GHz\n', f_r*1e-9)
-fprintf('W(f=f_r) = %.2f mm\n', W(f==f_r)*1e3)
-fprintf('L(f=f_r) = %.2f mm\n', L(f==f_r)*1e3)
+fprintf('W(f=f_r) = %.2f mm\n', W*1e3)
+fprintf('L(f=f_r) = %.2f mm\n', L*1e3)
 
 % 1.2
-% radiating slot admittance Y_s = G + iB
-G = W/(120*lambda0)*(1-(k0*h)^2/24);            % for h/lambda0 < 1/10
-B = W/(120*lambda0)*(1-0.636*log(k0*h));        % for 0.35 < W/lambda0 < 2
-Y_s = G + 1i*B;
-Z_s = 1./Y_s;
-
-% characteristic impedance Z_c of a microstrip
-if W/h <= 1
-    Z_c = 60/sqrt(epsR_eff)*log(8*h/W+W/(4*h));
-else
-    Z_c = 120*pi./(sqrt(epsR_eff).*(W/h+1.393+0.667*log(W/h+1.444)));
-end
-
-% input impedance Z_in as a function of:
-% L ... position of the feeding point from the edge of the antenna,
-% f ... operating frequency
-beta = 2*pi*sqrt(epsR_eff)/lambda0;
-[L1, L2, Z1, Z2] = deal(zeros(NLpoints, Nfpoints));
-for fi = 1:Nfpoints
-    L1(:,fi) = linspace(0, L(fi), NLpoints);
-    L2(:,fi) = L(fi) - L1(:, fi);
-    Z1(:,fi) = Z_c(fi).*(Z_s(fi)+1i*Z_c(fi).*tan(beta(fi).*L1(:,fi)))./(Z_c(fi)+1i.*Z_s(fi).*tan(beta(fi).*L1(:,fi)));
-    Z2(:,fi) = Z_c(fi).*(Z_s(fi)+1i*Z_c(fi).*tan(beta(fi).*L2(:,fi)))./(Z_c(fi)+1i.*Z_s(fi).*tan(beta(fi).*L2(:,fi)));
-end
-Z_in = 1./(1./Z1+1./Z2);
-
-% input impedance frequency plot for L1 == 0 mm
+% input impedance frequency plot for feed offset of 0 mm
+Z_in = impedanceTLM(epsR, h, W, L, 0, f);
 figure('Name', 'Input impedance of an edge-fed antenna')
-plot(f*1e-9, real(Z_in(1,:)))
+plot(f*1e-9, real(Z_in))
 hold on
-plot(f*1e-9, imag(Z_in(1,:)))
+plot(f*1e-9, imag(Z_in))
 xline(f_r*1e-9, '-.')
 yline(0)
 hold off
@@ -80,7 +55,7 @@ legend('R_{in}', 'X_{in}')
 
 % module of reflection coefficient frequency plot for L1 == 0 mm
 figure('Name', 'Reflection coefficient of an edge-fed antenna')
-plot(f*1e-9, 20*log10(abs((50-Z_in(1,:))./(50+Z_in(1,:)))))
+plot(f*1e-9, 20*log10(abs((50-Z_in)./(50+Z_in))))
 hold on
 xline(f_r*1e-9, '-.')
 hold off
@@ -94,13 +69,15 @@ ylabel('|\Gamma| [dB]')
 % 1.3
 % input impedance for L1 == 0 mm && f == f_r
 fprintf('\n1.3\n----\n')
-fprintf('Z_in(L1=0,f=f_r) = %.2f%+.2fi Ohm\n', real(Z_in(1,f==f_r)), imag(Z_in(1,f==f_r)))
+fprintf('Z_in(L1=0,f=f_r) = %.2f%+.2fi Ohm\n', real(Z_in(f==f_r)), imag(Z_in(f==f_r)))
 
-% input impedance feeding point position plot
+% input impedance feeding point position plot for f == f_r
+L1 = linspace(0, L, NLpoints);
+Z_in = impedanceTLM(epsR, h, W, L, L1, f_r);
 figure('Name', 'Input impedance at resonance of an offset-fed antenna')
-plot(L1(:,f==f_r)/L(f==f_r), real(Z_in(:,f==f_r)))
+plot(L1/L, real(Z_in))
 hold on
-plot(L1(:,f==f_r)/L(f==f_r), imag(Z_in(:,f==f_r)))
+plot(L1/L, imag(Z_in))
 line = yline(50, '--', '50');
 line.LabelVerticalAlignment = 'bottom';
 hold off
@@ -113,16 +90,17 @@ legend('R_{in}', 'X_{in}', 'Location', 'southeast')
 % title('Input impedance at resonant frequency')
 
 % find L for which R_in is approx. 50 Ohm at f == f_r
-[R_match, l_match] = min(abs(real(Z_in(:,f==f_r))-50));
+[R_match, L1_match] = min(abs(real(Z_in)-50));
 R_match = R_match + 50;
-disp(['L_match = ' num2str(round(L1(l_match,f==f_r)*1e3, 2)) ' mm'])
+disp(['L_match = ' num2str(round(L1(L1_match)*1e3, 2)) ' mm'])
 % disp(['R_match = ' num2str(round(R_match, 2)) ' Ohm'])
 
 % input impedance frequency plot for L == L_match
+Z_in = impedanceTLM(epsR, h, W, L, L1(L1_match), f);
 figure('Name', 'Input impedance of a matched antenna')
-plot(f*1e-9, real(Z_in(l_match,:)))
+plot(f*1e-9, real(Z_in))
 hold on
-plot(f*1e-9, imag(Z_in(l_match,:)))
+plot(f*1e-9, imag(Z_in))
 xline(f_r*1e-9, '-.')
 hold off
 axis tight
@@ -135,7 +113,7 @@ legend('R_{in}', 'X_{in}')
 
 % module of reflection coefficient frequency plot for L == L_match
 figure('Name', 'Reflection coefficient of a matched antenna')
-Gamma_match_dB = 20*log10(abs((50-Z_in(l_match,:))./(50+Z_in(l_match,:))));
+Gamma_match_dB = 20*log10(abs((50-Z_in./(50+Z_in))));
 plot(f*1e-9, Gamma_match_dB)
 hold on
 xline(f_r*1e-9, '-.')
@@ -204,8 +182,8 @@ ylabel('BW(VSWR<2) [%]')
 
 % 1.6
 theta = linspace(-pi/2, pi/2, 180);
-F_E = sin(k0*h/2.*cos(theta))./(k0*h/2.*cos(theta)).*cos(k0*L_eff(f==f_r)/2.*sin(theta));
-F_H = cos(theta).*sin(k0*h/2.*cos(theta))./(k0*h/2.*cos(theta)).*sin(k0*W(f==f_r)/2.*sin(theta))./(k0*W(f==f_r)/2.*sin(theta));
+F_E = sin(k0*h/2.*cos(theta))./(k0*h/2.*cos(theta)).*cos(k0*L_eff/2.*sin(theta));
+F_H = cos(theta).*sin(k0*h/2.*cos(theta))./(k0*h/2.*cos(theta)).*sin(k0*W/2.*sin(theta))./(k0*W/2.*sin(theta));
 
 figure('Name', 'Normalized elevation field pattern')
 polaraxes( ...
@@ -230,44 +208,57 @@ hold off
 % title('Normalized azimuthal field pattern')
 
 %% 2 Patch antenna measurement
-S11_4mm = sparameters(fullfile('data', '01 PATCH.S1P'));
-S11_16mm = sparameters(fullfile('data', '03 PATCH.S1P'));
-S11_28mm = sparameters(fullfile('data', '05 PATCH.S1P'));
-fMeas = S11_4mm.Frequencies;
-% NfpointsMeas = length(fMeas);
-% S11_4mm = squeeze(S11_4mm.Parameters);
-% S11_16mm = squeeze(S11_16mm.Parameters);
-% S11_28mm = squeeze(S11_28mm.Parameters);
+% % load measurement S-parameter data
+% S11_4mm = sparameters(fullfile('data', '01 PATCH.S1P'));
+% S11_16mm = sparameters(fullfile('data', '03 PATCH.S1P'));
+% S11_28mm = sparameters(fullfile('data', '05 PATCH.S1P'));
 
-L4mm = find(round(L1(:,f==f_r),3)==4e-3);
-L4mm = L4mm(ceil(end/2));
-L16mm = find(round(L1(:,f==f_r),3)==16e-3);
-L16mm = L16mm(ceil(end/2));
-L26mm = find(round(L1(:,f==f_r),3)==26e-3);
-L26mm = L26mm(ceil(end/2));
+% measurement parameters
+f_meas = sparameters(fullfile('data', '01 PATCH.S1P')).Frequencies;
+lambda0_meas = c./f_meas;
 
-figure(11)
-rfplot(S11_4mm)
-% rfplot(patch16mm)
-% rfplot(patch28mm)
-hold on
-plot(f*1e-9, 20*log10(abs((50-Z_in(L4mm,:))./(50+Z_in(L4mm,:)))))
-hold off
-xlabel('Frequency [GHz]')
-ylabel('|S_{11}| [dB]')
-legend('Measured data', 'TLM', 'Location', 'southeast')
-title('')
+% measured antenna properties
+epsR_meas = 1;
+h_meas = 5e-3;
+L_meas = 58e-3;
+W_meas = 61e-3;
+
+L1_meas = [4e-3; 16e-3; 28e-3];
+S11_meas = [
+    squeeze(sparameters(fullfile('data', '01 PATCH.S1P')).Parameters(1,1,:)), ...
+    squeeze(sparameters(fullfile('data', '03 PATCH.S1P')).Parameters(1,1,:)), ...
+    squeeze(sparameters(fullfile('data', '05 PATCH.S1P')).Parameters(1,1,:))
+];
+Z_in_meas = [
+    impedanceTLM(epsR_meas, h_meas, W_meas, L_meas, L1_meas(1), S11_meas(1)), ...
+    impedanceTLM(epsR_meas, h_meas, W_meas, L_meas, L1_meas(2), S11_meas(2)), ...
+    impedanceTLM(epsR_meas, h_meas, W_meas, L_meas, L1_meas(3), S11_meas(3))
+];
+
+% figure(11)
+% rfplot(S11_4mm)
+% hold on
+% plot(f*1e-9, 20*log10(abs((50-Z_in(L4mm,:))./(50+Z_in(L4mm,:)))))
+% hold off
+% xlabel('Frequency [GHz]')
+% ylabel('|S_{11}| [dB]')
+% legend('Measured data', 'TLM', 'Location', 'southeast')
+% title('')
 
 %% Functions
-
-function Z_in = impedanceTLM(epsR, h, W, L, L1, f)
+% Calculation of input impedance using the Transmission Line Model
+function Z_in = impedanceTLM(epsR, h, W, L, feedOffset, f)
+    % universal quantities
+    c = 299792458;
     lambda0 = c./f;
     k0 = 2*pi./lambda0;
+
+    % effective permittivity
     epsR_eff = (epsR+1)/2+(epsR-1)/2*(1+12*h./W).^(-1/2);
 
     % radiating slot admittance Y_s = G + iB
-    G = W/(120*lambda0)*(1-(k0*h)^2/24);            % for h/lambda0 < 1/10
-    B = W/(120*lambda0)*(1-0.636*log(k0*h));        % for 0.35 < W/lambda0 < 2
+    G = W./(120*lambda0).*(1-(k0*h).^2/24);             % for h/lambda0 < 1/10
+    B = W./(120*lambda0).*(1-0.636*log(k0*h));          % for 0.35 < W/lambda0 < 2
     Y_s = G + 1i*B;
     Z_s = 1./Y_s;
     
@@ -281,9 +272,8 @@ function Z_in = impedanceTLM(epsR, h, W, L, L1, f)
     % input impedance Z_in as a function of:
     % L ... position of the feeding point from the edge of the antenna,
     % f ... operating frequency
-    beta = 2*pi*sqrt(epsR_eff)/lambda0;
-    L2 = L - L1;
-    Z1 = Z_c.*(Z_s+1i*Z_c.*tan(beta*L1))./(Z_c+1i.*Z_s.*tan(beta*L1));
-    Z2 = Z_c.*(Z_s+1i*Z_c.*tan(beta*L2))./(Z_c+1i.*Z_s.*tan(beta*L2));
+    beta = 2*pi*sqrt(epsR_eff)./lambda0;
+    Z1 = Z_c.*(Z_s+1i*Z_c.*tan(beta*feedOffset))./(Z_c+1i*Z_s.*tan(beta*feedOffset));
+    Z2 = Z_c.*(Z_s+1i*Z_c.*tan(beta*(L-feedOffset)))./(Z_c+1i*Z_s.*tan(beta*(L-feedOffset)));
     Z_in = 1./(1./Z1+1./Z2);
 end
